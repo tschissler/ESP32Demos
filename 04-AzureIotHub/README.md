d1cedef98dd791b79fd03c1addc6bf7cf6f85a9e
ThomasSchissler <tschissler@agilemax.de>
2021-05-31 15:52:06 +0200

    Added AzureIoTHub demo

---
+++ 04-AzureIotHub/README.md
@@ -0,0 +1,18 @@
+# Azure IoT Hub Demo
+In this demo we connect our device to Azure IoT Hub via WiFi and push sensor data to the cloud. 
+At the same time we allow to update values for the RGB LED on the digital twin which are then send from the cloud to the device.
+For this demo it can be helpful to install the Azure IoT Hub extension in VS Code (https://marketplace.visualstudio.com/items?itemName=vsciot-vscode.azure-iot-toolkit).
+
+You need an active Azure Subscription. If you do not have one, you can create a new one which comes with plenty of free offers and a $200 budget (https://azure.microsoft.com/en-us/free/).
+
+In the subscription you have to create a new IoT Hub. You can do so via the Azure portal (https://portal.azure.com) or the Azure IoT Hub Extension in VS Code.
+
+
+For this demo the F1: Free tier should be sufficient.
+
+In the IoT Hub create a new device. Use the connectionString in your code to define the device you want to connect to. Also set the WiFi parameters accordingly.
+
+1. EventHub 
+1. Add Custom Endpoint
+1. Route Message to Storage Endpoint
+
---
+++ 04-AzureIotHub/src/main.cpp
@@ -0,0 +1,217 @@
+// Copyright (c) Microsoft. All rights reserved.
+// Licensed under the MIT license. 
+
+#include <WiFi.h>
+#include "AzureIotHub.h"
+#include "Esp32MQTTClient.h"
+#include <Adafruit_Sensor.h>
+#include <DHT.h>
+#include <DHT_U.h>
+
+#define INTERVAL 10000
+#define DEVICE_ID "Esp32Device"
+#define MESSAGE_MAX_LEN 256
+
+#define DHTPIN 17
+// Uncomment the type of sensor in use:
+#define DHTTYPE    DHT11     // DHT 11
+//#define DHTTYPE    DHT22     // DHT 22 (AM2302)
+//#define DHTTYPE    DHT21     // DHT 21 (AM2301)
+DHT_Unified dht(DHTPIN, DHTTYPE);
+
+// Please input the SSID and password of WiFi
+const char* ssid     = "<Type SSID here>";
+const char* password = "<Type Password here>";
+
+/*String containing Hostname, Device Id & Device Key in the format:                         */
+/*  "HostName=<host_name>;DeviceId=<device_id>;SharedAccessKey=<device_key>"                */
+/*  "HostName=<host_name>;DeviceId=<device_id>;SharedAccessSignature=<device_sas_token>"    */
+static const char* connectionString = "<Type connectionString here>";
+
+const char *messageData = "{\"deviceId\":\"%s\", \"messageId\":%d, \"Temperature\":%f, \"Humidity\":%f}";
+
+int messageCount = 1;
+static bool hasWifi = false;
+static bool messageSending = true;
+static uint64_t send_interval_ms;
+
+
+//////////////////////////////////////////////////////////////////////////////////////////////////////////
+// Utilities
+static void InitWifi()
+{
+  Serial.println("Connecting...");
+  WiFi.begin(ssid, password);
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+  }
+  hasWifi = true;
+  Serial.println("WiFi connected");
+  Serial.println("IP address: ");
+  Serial.println(WiFi.localIP());
+}
+
+static void SendConfirmationCallback(IOTHUB_CLIENT_CONFIRMATION_RESULT result)
+{
+  if (result == IOTHUB_CLIENT_CONFIRMATION_OK)
+  {
+    Serial.println("Send Confirmation Callback finished.");
+  }
+}
+
+static void MessageCallback(const char* payLoad, int size)
+{
+  Serial.println("Message callback:");
+  Serial.println(payLoad);
+}
+
+static void DeviceTwinCallback(DEVICE_TWIN_UPDATE_STATE updateState, const unsigned char *payLoad, int size)
+{
+  char *temp = (char *)malloc(size + 1);
+  if (temp == NULL)
+  {
+    return;
+  }
+  memcpy(temp, payLoad, size);
+  temp[size] = '\0';
+  // Display Twin message.
+  Serial.println(temp);
+  free(temp);
+}
+
+static int  DeviceMethodCallback(const char *methodName, const unsigned char *payload, int size, unsigned char **response, int *response_size)
+{
+  LogInfo("Try to invoke method %s", methodName);
+  const char *responseMessage = "\"Successfully invoke device method\"";
+  int result = 200;
+
+  if (strcmp(methodName, "start") == 0)
+  {
+    LogInfo("Start sending temperature and humidity data");
+    messageSending = true;
+  }
+  else if (strcmp(methodName, "stop") == 0)
+  {
+    LogInfo("Stop sending temperature and humidity data");
+    messageSending = false;
+  }
+  else
+  {
+    LogInfo("No method %s found", methodName);
+    responseMessage = "\"No method found\"";
+    result = 404;
+  }
+
+  *response_size = strlen(responseMessage) + 1;
+  *response = (unsigned char *)strdup(responseMessage);
+
+  return result;
+}
+
+//////////////////////////////////////////////////////////////////////////////////////////////////////////
+// Arduino sketch
+void setup()
+{
+  Serial.begin(115200);
+  Serial.println("ESP32 Device");
+  Serial.println("Initializing...");
+
+  // Initialize the WiFi module
+  Serial.println(" > WiFi");
+  hasWifi = false;
+  InitWifi();
+  if (!hasWifi)
+  {
+    return;
+  }
+  randomSeed(analogRead(0));
+
+  Serial.println(" > IoT Hub");
+  Esp32MQTTClient_SetOption(OPTION_MINI_SOLUTION_NAME, "GetStarted");
+  Esp32MQTTClient_Init((const uint8_t*)connectionString, true);
+
+  Esp32MQTTClient_SetSendConfirmationCallback(SendConfirmationCallback);
+  Esp32MQTTClient_SetMessageCallback(MessageCallback);
+  Esp32MQTTClient_SetDeviceTwinCallback(DeviceTwinCallback);
+  Esp32MQTTClient_SetDeviceMethodCallback(DeviceMethodCallback);
+
+  send_interval_ms = millis();
+
+  dht.begin();
+  Serial.println(F("DHTxx Unified Sensor Example"));
+  // Print temperature sensor details.
+  sensor_t sensor;
+  dht.temperature().getSensor(&sensor);
+  Serial.println(F("------------------------------------"));
+  Serial.println(F("Temperature Sensor"));
+  Serial.print  (F("Sensor Type: ")); Serial.println(sensor.name);
+  Serial.print  (F("Driver Ver:  ")); Serial.println(sensor.version);
+  Serial.print  (F("Unique ID:   ")); Serial.println(sensor.sensor_id);
+  Serial.print  (F("Max Value:   ")); Serial.print(sensor.max_value); Serial.println(F("°C"));
+  Serial.print  (F("Min Value:   ")); Serial.print(sensor.min_value); Serial.println(F("°C"));
+  Serial.print  (F("Resolution:  ")); Serial.print(sensor.resolution); Serial.println(F("°C"));
+  Serial.println(F("------------------------------------"));
+  // Print humidity sensor details.
+  dht.humidity().getSensor(&sensor);
+  Serial.println(F("Humidity Sensor"));
+  Serial.print  (F("Sensor Type: ")); Serial.println(sensor.name);
+  Serial.print  (F("Driver Ver:  ")); Serial.println(sensor.version);
+  Serial.print  (F("Unique ID:   ")); Serial.println(sensor.sensor_id);
+  Serial.print  (F("Max Value:   ")); Serial.print(sensor.max_value); Serial.println(F("%"));
+  Serial.print  (F("Min Value:   ")); Serial.print(sensor.min_value); Serial.println(F("%"));
+  Serial.print  (F("Resolution:  ")); Serial.print(sensor.resolution); Serial.println(F("%"));
+  Serial.println(F("------------------------------------"));
+  // Set delay between sensor readings based on sensor details.
+
+}
+
+void loop()
+{
+  if (hasWifi)
+  {
+    if (messageSending && 
+        (int)(millis() - send_interval_ms) >= INTERVAL)
+    {
+      // Send teperature data
+      char messagePayload[MESSAGE_MAX_LEN];
+
+      sensors_event_t event;
+      dht.temperature().getEvent(&event);
+      if (isnan(event.temperature)) {
+        Serial.println(F("Error reading temperature!"));
+      }
+      else {
+        Serial.print(F("Temperature: "));
+        Serial.print(event.temperature);
+        Serial.println(F("°C"));
+      }
+      float temperature = event.temperature;
+
+      // Get humidity event and print its value.
+      dht.humidity().getEvent(&event);
+      if (isnan(event.relative_humidity)) {
+        Serial.println(F("Error reading humidity!"));
+      }
+      else {
+        Serial.print(F("Humidity: "));
+        Serial.print(event.relative_humidity);
+        Serial.println(F("%"));
+      }
+      float humidity = event.relative_humidity
+      ;
+      snprintf(messagePayload,MESSAGE_MAX_LEN, messageData, DEVICE_ID, messageCount++, temperature,humidity);
+      Serial.println(messagePayload);
+      EVENT_INSTANCE* message = Esp32MQTTClient_Event_Generate(messagePayload, MESSAGE);
+      Esp32MQTTClient_Event_AddProp(message, "temperatureAlert", "true");
+      Esp32MQTTClient_SendEventInstance(message);
+      
+      send_interval_ms = millis();
+    }
+    else
+    {
+      Esp32MQTTClient_Check();
+    }
+  }
+  delay(10);
+}
