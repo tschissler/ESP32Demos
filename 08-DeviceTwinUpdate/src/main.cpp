5a02ed25d6c30b6bac583500655b99d3069519d3
Thomas Schissler <thomas@agilemax.de>
2021-06-06 14:09:41 +0200

    Changed initial LED color to blue

--- 08-DeviceTwinUpdate/src/main.cpp
+++ 08-DeviceTwinUpdate/src/main.cpp
@@ -23,9 +23,9 @@
 #define greenChannel 1
 #define blueChannel 2
 
-int redBrightness = 255;
+int redBrightness = 0;
 int greenBrightness = 0;
-int blueBrightness = 0;
+int blueBrightness = 200;
 
 #define pwm_freq 5000
 #define pwm_resolution 8
